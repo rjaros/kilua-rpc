122a03e8fd507e4406cf62c52e68eb68363f040b
Robert Jaros <rjaros@finn.pl>
2024-06-18 07:46:24 +0200

    Simplify examples configuration

--- examples/fullstack-javalin/webpack.config.d/webpack.js
+++ examples/fullstack-javalin/webpack.config.d/webpack.js
@@ -1,23 +1,9 @@
-config.resolve.modules.push("../../processedResources/js/main");
-config.resolve.modules.push("../../processedResources/wasmJs/main");
-
 if (config.devServer) {
     config.devServer.hot = true;
     config.devServer.open = false;
     config.devServer.port = 3000;
     config.devServer.historyApiFallback = true;
     config.devtool = 'eval-cheap-source-map';
-    config.devServer.proxy = [
-        {
-            context: ["/rpc/*", "/rpcsse/*"],
-            target: 'http://localhost:8080'
-        },
-        {
-            context: ["/rpcws/*"],
-            target: 'http://localhost:8080',
-            ws: true
-        }
-    ]
 } else {
     config.devtool = undefined;
 }
