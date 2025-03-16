7b5437ce447f3fcf27f4829ef9cbc0b5c2277a1d
Robert Jaros <rjaros@finn.pl>
2025-03-16 07:45:10 +0100

    New example application for pure Ktor module (#1)

---
+++ examples/fullstack-ktor/build.gradle.kts
@@ -0,0 +1,84 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("com.google.devtools.ksp")
+    alias(libs.plugins.kilua.rpc)
+}
+
+val mainClassName = "io.ktor.server.netty.EngineMain"
+
+@OptIn(ExperimentalWasmDsl::class)
+kotlin {
+    jvmToolchain(21)
+    jvm {
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+        }
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        mainRun {
+            mainClass.set(mainClassName)
+        }
+    }
+    js(IR) {
+        useEsModules()
+        browser {
+            commonWebpackConfig {
+                outputFileName = "main.bundle.js"
+            }
+            runTask {
+                sourceMaps = false
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    wasmJs {
+        useEsModules()
+        browser {
+            commonWebpackConfig {
+                outputFileName = "main.bundle.js"
+            }
+            runTask {
+                sourceMaps = false
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kilua.rpc.ktor)
+                implementation(libs.kotlinx.datetime)
+            }
+        }
+        val webMain by creating {
+            dependsOn(commonMain)
+        }
+        val jsMain by getting {
+            dependsOn(webMain)
+        }
+        val wasmJsMain by getting {
+            dependsOn(webMain)
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(libs.ktor.server.netty)
+                implementation(libs.ktor.server.compression)
+                implementation(libs.logback.classic)
+            }
+        }
+    }
+}
